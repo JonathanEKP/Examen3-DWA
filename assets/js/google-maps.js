3d7c61eed4f936cdd42cd77e7297234bab4920d1
RodriZombi <rodrigoportillo03@gmail.com>
2022-11-05 19:40:36 -0600

    cambios en mapa api de google maps

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,15 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 13.68030098061711, lng: -89.26915422485176};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
-        zoom: 17,
+        zoom: 12,
         center: myLatLng
     });
 
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'La Libertad, El Salvador' // Title Location
     });
 }
